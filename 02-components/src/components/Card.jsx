edb6c2ad6530e8b81aaf856a4a0d1be88209ad8a
Adarsh-17-raj <22054211@kiit.ac.in>
2026-05-11 01:55:49 +0530

    component file created

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,11 @@
+
+function Card() {
+  return (
+    <div className='cd'>
+        <h1>Hii guyss</h1>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, necessitatibus?</h3>
+    </div>
+  )
+}
+
+export default Card;
---
+++ 02-components/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className='nv'>
+      <h1>this is a navbar</h1>
+    </div>
+  )
+}
+
+export default navbar
