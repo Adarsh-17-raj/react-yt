65e7287dad35dcb87481b10b91c3bfaf9c05d716
Adarsh-17-raj <22054211@kiit.ac.in>
2026-05-11 00:47:45 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+
+const App = () => {
+  return (
+    <>
+      <div id="DAD">
+        <h1 id="C1">Hello</h1>
+        <h3 id="C2">guyss</h3>
+      </div>
+      <div id="chacha"></div>
+    </>
+  )
+
+}
+
+export default App
